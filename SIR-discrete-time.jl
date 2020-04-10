b012b4ccb1a39bade5bec492d4f5db0b2c2f30b6
Oskar Pfeffer <o.pfeffer@pik-potsdam.de>
2020-04-10 13:44:13 +0200

    Working example of metropolis

--- SIR-discrete-time.jl
+++ SIR-discrete-time.jl
@@ -1,74 +1,85 @@
 using LightGraphs
 using BenchmarkTools
 
-g = barabasi_albert(1000, 10)
 
-infected_initial = [false for v in vertices(g)]
-infected_initial[1] = true
-recovered_initial = [false for v in vertices(g)]
+# I :: Infected, R :: Recovered
 
-function step2!(infected, has_been_infected, g)
-    for v in infected
+function step2!(g, I, has_been_I, β, γ)
+    N_inf = length(I)
+    for v in I[1:N_inf]
         # Infect neighbours
         for n in neighbors(g, v)
-            if (! has_been_infected[n]) && rand() < 0.3
-                push!(infected, n)
-                has_been_infected[n] = true
+            if (! has_been_I[n]) && rand() < β
+                push!(I, n)
+                has_been_I[n] = true
             end
         end
     end
+    # for v,i in enumerate(I[N_inf:-1:1])
+    recovering = falses(length(I))
+    recovering[1:N_inf] = map(x -> rand() < 0.3, 1:N_inf)
+    deleteat!(I, recovering)
 
-    for (i, v) in enumerate(infected)
-        # recover and remove all copies of the infection
-        if rand() < 0.2
-            # recovered[v] = true
-            deleteat!(infected, i)
-        end
-    end
 end
 
-function epidemic2(infected_initial, hbi_initial, g, N)
-    infected = copy(infected_initial)
-    hbi = copy(hbi_initial)
+function epidemic2(g, I, N; β=0.01, γ=0.2)
+    hbi = falses(nv(g))
+    max_I = length(I)
     for i in 1:N
-        step2!(infected, hbi, g)
+        step2!(g, I, hbi, β, γ)
+        max_I = max(length(I), max_I)
     end
-    # println("Have been infected: $(sum(hbi)); Infected: $(length(infected))")
+    # Returns maximum number of simultaneously infected, number of infected after
+    # last step and total number of those who have been infected
+    max_I, length(I), sum(hbi)
 end
 
 
-function step!(infected, recovered, g)
+function step!(I, R, g)
     for v in vertices(g)
-        if infected[v]
+        if I[v]
             # Infect neighbours
             for n in neighbors(g, v)
-                if (! infected[n]) && (! recovered[n]) && rand() < 0.3
-                    #println("infected node $n")
-                    infected[n] = true
+                if (! I[n]) && (! R[n]) && rand() < 0.3
+                    #println("I node $n")
+                    I[n] = true
                 end
             end
 
             # recover
             if rand() < 0.2
-                #println("node $v recovered")
-                infected[v] = false
-                recovered[v] = true
+                #println("node $v R")
+                I[v] = false
+                R[v] = true
             end
         end
     end
 end
 
-function epidemic(infected_initial, recovered_initial, g, N)
-    infected = copy(infected_initial)
-    recovered = copy(recovered_initial)
+function epidemic(I_initial, R_initial, g, N)
+    I = copy(I_initial)
+    R = copy(R_initial)
+    max_I = length(I)
+
     for i in 1:N
-        step!(infected, recovered, g)
+        step!(I, R, g)
+        max_I = max(sum(I), max_I)
     end
-    # println("Recovered: $(sum(recovered)); Infected: $(sum(infected))")
+    max_I
+    # println("Recovered: $(sum(R)); Infected: $(sum(I))")
 end
 
-epidemic(infected_initial, recovered_initial, g, 20)
-@btime epidemic(infected_initial, recovered_initial, g, 20)
 
-epidemic2([1], recovered_initial, g, 20)
-@btime epidemic2([1], recovered_initial, g, 20)
+g = barabasi_albert(1000, 10)
+
+I_initial = falses(nv(g))
+I_initial[1] = true
+R_initial = falses(nv(g))
+
+
+epidemic(I_initial, R_initial, g, 1)
+@btime epidemic(I_initial, R_initial, g, 20)
+
+I_initial = rand(1:nv(g), 10)
+epidemic2(g, copy(I_initial), 100, β=0.07, γ=0.02)
+@btime epidemic2(g, copy(I_initial), 100, β=0.07, γ=0.02)
---
+++ anneal_SIR_dt.jl
@@ -0,0 +1,139 @@
+using LightGraphs
+
+"""
+Initializes a given graph g by cutting cut_size edges randomly. Returns
+edge list of cut and still existing edges.
+"""
+function initialize_graph!(g, cut_size)
+    EL = collect(edges(g))
+    is_in_graph = trues(ne(g))          # true if edge i of EL is in graph
+    cut_EL = typeof(EL)(undef, cut_size)     # List of edges cut from the graph
+    existing_EL = copy(EL)              # List of edges present in the graph
+
+    index_list = zeros(Integer, cut_size)
+    for i in 1:cut_size
+        index = rand(1:length(EL))
+        while !is_in_graph[index]
+            index = rand(1:length(EL))
+        end
+        is_in_graph[index] = 0
+        index_list[i] = index
+        cut_EL[i] = EL[index]
+        rem_edge!(g, EL[index])
+    end
+    deleteat!(existing_EL, sort!(index_list))
+    g, cut_EL, existing_EL
+end
+
+
+"""
+MC step function, swaps one edge from the cut_EL with the existing_EL and also
+removes/adds the edges in the graph.
+"""
+function mc_step_SIR!(g, ρ_func, ρ_old, existing_EL, cut_EL; ν=1, args...)
+    cut_size = length(cut_EL)
+    # choose index of edges to be swapped
+    index_existing = rand(1:(ne(g)-cut_size))
+    index_cut = rand(1:cut_size)
+    # select edges to be swapped
+    cut_edge = cut_EL[index_cut]                    # edge that was cut and is added to the graph
+    existing_edge = existing_EL[index_existing]     # edge that was in the graph and is cut from it
+    # energy = energy_function(g, 1000)[1] # Maybe no need to calculate energy again
+    # ρ_old = ρ(g, args...)
+
+    rem_edge!(g, existing_edge)
+    add_edge!(g, cut_edge)
+    ρ_new = ρ_func(g; args...)
+    if (ρ_new/ρ_old)^ν < rand() # if ... reject proposal. exponent ν to flatten the pdf
+        add_edge!(g, existing_edge)
+        rem_edge!(g, cut_edge)
+        return ρ_old            # return ρ so it is not calculated again
+    else
+        # swap the two edges in the lists
+        cut_EL[index_cut] = existing_edge
+        existing_EL[index_existing] = cut_edge
+
+        return ρ_new            # return ρ so it is not calculated again
+    end
+end
+
+
+function ρ_epidemic(g; β, γ, tsteps, I_initial, args...)
+    epidemic2(g, rand(1:nv(g),I_initial), tsteps; β=β, γ=γ)[1] ^ -1.
+end
+
+
+
+function metropolis!(graph::AbstractGraph, ρ_func::Function, mc_steps::Integer;
+    mc_step!::Function=mc_step_SIR!,  existing_EL, cut_EL, args...)
+    ρ = ρ_func(g; args...)
+    for step in 1:mc_steps
+        ρ = mc_step!(g, ρ_func, ρ, existing_EL, cut_EL;args...)
+    end
+    graph
+end
+
+
+# WORKING EXAMPLE
+g = barabasi_albert(100,10)
+g, cut_EL, existing_EL = initialize_graph!(g, 10)
+β = 0.02; γ = 0.02; tsteps=100; I_initial=10
+ρ_temp = ρ_epidemic(g, β=β, γ=γ, tsteps=tsteps, I_initial=I_initial)
+mc_step_SIR!(g, ρ_epidemic, ρ_temp, existing_EL, cut_EL; β=β, γ=γ, tsteps=tsteps, I_initial=I_initial)
+
+metropolis!(g, ρ_epidemic, 100000, existing_EL=existing_EL, cut_EL=cut_EL; β=β, γ=γ, tsteps=tsteps, I_initial=I_initial, ν=1)
+
+#Check if number of max_infected_mean decreases after metropolis
+
+N = 10000
+max_infected_mean=0
+for i in 1:N
+    global max_infected_mean += epidemic2(g, rand(1:nv(g),10), 100, β=0.02, γ=0.02)[1]
+end
+max_infected_mean /= N
+
+
+
+
+
+"""
+This functions executes a simulated annealing of a given graph.
+"""
+function anneal!(graph::AbstractGraph, ρ_func::Function, mc_steps::Integer;
+    data::AbstractArray=[], data! =collect_data_energy!, annealing_schedule::Function=ann_sched_mult_exp,
+    mc_step!::Function=mc_step_SIR!,
+    temperature_changes::Integer=100, β_0::Float64=0., existing_EL, cut_EL, args...)
+    energy = energy_function(g,100)[1]
+    for step in 1:mc_steps
+        β = annealing_schedule(round(Integer, temperature_changes * step/mc_steps); args...)
+        ρ = mc_step!(g, ρ_func, existing_EL, cut_EL, ρ, β)
+        data!(data; graph=graph, energy=energy, step=step, args...)
+    end
+    graph
+end
+data = zeros(1000)
+
+g = anneal!(g, energy_SIR_dt, 1000, existing_EL=existing_EL, cut_EL=cut_EL, data=data)
+data
+energy_SIR_dt(g,1000)
+
+ann_sched_mult_exp(k::Integer; T_0::Real=1., T_n::Real=0., α::Real=0.95, args...) = 1. / (T_0 * α^k + T_n)
+
+function energy_SIR_dt(g, N_simulations)
+    energy= 0
+    energy2=0
+    for i in 1:N_simulations
+        temp = epidemic2(g, [rand(1:nv(g))], 20, β=0.08)[1]
+        energy+=temp
+        energy2+=temp^2
+    end
+    energy/N_simulations, sqrt(energy2/N_simulations - (energy/N_simulations)^2)
+end
+
+""" this function puts the energy in a given data array every collect_every steps"""
+function collect_data_energy!(data::AbstractArray; energy::Real, step::Int, collect_every::Int=1, args...)
+    if (step % collect_every) == 0
+        index = div(step, collect_every)
+        data[index] = energy
+    end
+end
