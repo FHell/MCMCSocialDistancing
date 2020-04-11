c5e826e006b21e2e0f4d4b8eeca1a181d5bace27
hellmann <hellmann@pik-potsdam.de>
2020-04-11 18:48:42 +0200

    Random distribution of \rho for comparison

--- sample_inverse_mean_mi.jl
+++ sample_inverse_mean_mi.jl
@@ -20,6 +20,14 @@ cut_ratio = 0.2
 cut_g, cut_EL = initialize_graph!(g, Int(round(cut_ratio * ne(g))))
 β = 0.02; γ = 0.02; tsteps=100; n_initial=10
 ρ_temp = ρ_epidemic(cut_g, β=β, γ=γ, tsteps=tsteps, n_initial=n_initial)
+
+Nref = 100
+s_ρ_rand = zeros(Nref)
+for i in 1:Nref
+    cut_g, cut_EL = initialize_graph!(g, Int(round(cut_ratio * ne(g))))
+    s_ρ_rand[i] = ρ_epidemic(cut_g, β=β, γ=γ, tsteps=tsteps, n_initial=n_initial)
+end
+
 mc_step_SIR!(cut_g, cut_EL, ρ_epidemic, ρ_temp; β=β, γ=γ, tsteps=tsteps, n_initial=n_initial)
 metropolis!(cut_g, cut_EL, ρ_epidemic, Int(1E2); β=β, γ=γ, tsteps=tsteps, n_initial=n_initial, ν=5)
 
@@ -37,5 +45,6 @@ sample_array = [metropolis!(cut_g, cut_EL, ρ_epidemic, sample_interval; β=β, 
 
 s_ρ_wu = [s[3] for s in sample_array_wu]
 s_ρ = [s[3] for s in sample_array]
+println((mean(s_ρ_rand), mean(s_ρ_rand)+std(s_ρ_rand), mean(s_ρ_rand)-std(s_ρ_rand)))
 println((mean(s_ρ_wu), mean(s_ρ_wu)+std(s_ρ_wu), mean(s_ρ_wu)-std(s_ρ_wu)))
 println((mean(s_ρ), mean(s_ρ)+std(s_ρ), mean(s_ρ)-std(s_ρ)))
