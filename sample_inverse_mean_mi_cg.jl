a36c0c4b66cc7915670c142fff1efd8f8e94e45a
hellmann <hellmann@pik-potsdam.de>
2020-04-17 09:41:15 +0200

    Complete graph

---
+++ sample_inverse_mean_mi_cg.jl
@@ -0,0 +1,56 @@
+# This file generates
+
+
+import Pkg
+Pkg.activate(".")
+# Pkg.instantiate()
+
+using LightGraphs
+using Statistics
+include("SIR-discrete-time_fh.jl")
+include("Metropolis_sampling_cuts.jl")
+
+function ρ_epidemic(g; β, γ, tsteps, n_initial, args...)
+    exp(-1. * mean([epidemic(g, rand(1:nv(g),n_initial), tsteps; β=β, γ=γ)[1] for i in 1:1000])/30.)
+end
+
+
+g = complete_graph(100)
+cut_ratio = 0.95
+cut_g, cut_EL = initialize_graph!(g, Int(round(cut_ratio * ne(g))))
+β = 0.02; γ = 0.02; tsteps=100; n_initial=10
+ρ_temp = ρ_epidemic(cut_g, β=β, γ=γ, tsteps=tsteps, n_initial=n_initial)
+
+Nref = 100
+s_ρ_rand = zeros(Nref)
+for i in 1:Nref
+    cut_g, cut_EL = initialize_graph!(g, Int(round(cut_ratio * ne(g))))
+    s_ρ_rand[i] = ρ_epidemic(cut_g, β=β, γ=γ, tsteps=tsteps, n_initial=n_initial)
+end
+
+#mc_step_SIR!(cut_g, cut_EL, ρ_epidemic, ρ_temp; β=β, γ=γ, tsteps=tsteps, n_initial=n_initial)
+#metropolis!(cut_g, cut_EL, ρ_epidemic, Int(1E2); β=β, γ=γ, tsteps=tsteps, n_initial=n_initial, ν=1E4)
+
+sample_interval = Int(1E2)
+if "long" in ARGS
+    println("Long run")
+    sample_interval *= 100
+end
+
+# Temperature Schedule:
+sample_array_wu1 = [metropolis!(cut_g, cut_EL, ρ_epidemic, sample_interval; β=β, γ=γ, tsteps=tsteps, n_initial=n_initial, ν=1E1) for i in 1:50]
+sample_array_wu2 = [metropolis!(cut_g, cut_EL, ρ_epidemic, sample_interval; β=β, γ=γ, tsteps=tsteps, n_initial=n_initial, ν=1E2) for i in 1:50]
+sample_array_wu3 = [metropolis!(cut_g, cut_EL, ρ_epidemic, sample_interval; β=β, γ=γ, tsteps=tsteps, n_initial=n_initial, ν=1E3) for i in 1:50]
+sample_array_wu4 = [metropolis!(cut_g, cut_EL, ρ_epidemic, sample_interval; β=β, γ=γ, tsteps=tsteps, n_initial=n_initial, ν=1E4) for i in 1:500]
+
+using BSON
+
+i = 1
+while isfile("data_cg_$i.bson")
+    global i
+    i += 1
+end
+
+bson("data_cg_$i.bson", g = g, sa = sample_array_wu4, cut_ratio = cut_ratio, β = β, γ = γ, tsteps=tsteps, n_initial=n_initial, sample_interval=sample_interval)
+
+println(mean[-log(s[3])*30. for s in sample_array_wu4])
---
+++ submit_cg.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --qos=priority
+#SBATCH --job-name=dev_run
+#SBATCH --account=coen
+#SBATCH --output=MCMCepi-%j-%N.out
+#SBATCH --error=MCMCepi-%j-%N.err
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mail-type=END
+#SBATCH --mail-user=hellmann
+
+echo "------------------------------------------------------------"
+echo "SLURM JOB ID: $SLURM_JOBID"
+echo "$SLURM_NTASKS tasks"
+echo "------------------------------------------------------------"
+
+module load julia/1.3.0
+module load hpc/2015
+julia sample_inverse_mean_mi_cg.jl
